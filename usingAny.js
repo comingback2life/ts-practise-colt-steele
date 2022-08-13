d3b21cb0b290771fd96344e617c88f4dd463c6ec
Samip <callmesamip@gmail.com>
2022-08-13 20:12:37 +1000

    Learned about inferring types and the any type

---
+++ usingAny.js
@@ -0,0 +1,9 @@
+var movies = ['Batman', 'Batman 2', 'The Thing'];
+var foundMovie;
+for (var _i = 0, movies_1 = movies; _i < movies_1.length; _i++) {
+    var movie = movies_1[_i];
+    if (movie === 'Batman') {
+        foundMovie = movie;
+    }
+}
+console.log(foundMovie);
---
+++ usingAny.ts
@@ -0,0 +1,9 @@
+const movies = ['Batman', 'Batman 2', 'The Thing'];
+
+let foundMovie: string;
+
+for (let movie of movies) {
+	if (movie === 'Batman') {
+		foundMovie = movie;
+	}
+}
