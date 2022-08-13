cb07abc4f727d863a553998f0d0cee737dda2a33
Samip <callmesamip@gmail.com>
2022-08-13 21:32:38 +1000

    type-aliases

---
+++ type-aliases/typeAliasPrac.js
@@ -0,0 +1,36 @@
+/**
+ * Rather than supplying the object type in an annotation we can make use of type aliases
+ */
+var doublePoint = function (point) {
+    //this functions accepts an object called point which has x as a number and y as a number
+    //this function should return object according to TS, so it will return another object where x and y are both numbers
+    return { x: point.x * 2, y: point.y * 2 };
+};
+var doublePointv2 = function (point) {
+    //this now accepts a object called point that is of type Point and returns an object that is of type Point
+    return { x: point.x * 2, y: point.y * 2 };
+};
+//nested object
+var calculateEarnings = function (song) {
+    return song.numberOfStreams * 0.333;
+}; // rather than doing this we can just pass the types
+var calculateEarningsv2 = function (song) {
+    //refactored version of the function above using types
+    return song.numberOfStreams * 0.333;
+};
+var printSong = function (mySong) {
+    //mySong is a function param with the type Song
+    console.log("".concat(mySong.title, " - ").concat(mySong.artist));
+};
+var mySong = {
+    title: 'I want to break free',
+    artist: 'Queen',
+    numberOfStreams: 123456765432,
+    credits: {
+        producer: "I don't know",
+        writer: "I don't know writer"
+    }
+};
+var earnings = calculateEarningsv2(mySong);
+console.log(earnings);
+printSong(mySong);
--- type-aliases/typeAliasPrac.ts
+++ type-aliases/typeAliasPrac.ts
@@ -7,6 +7,16 @@ type Point = {
 	y: number;
 };
 
+type Song = {
+	title: 'I want to break free';
+	artist: 'Queen';
+	numberOfStreams: 123456765432;
+	credits: {
+		producer: "I don't know";
+		writer: "I don't know writer";
+	};
+};
+
 const doublePoint = (point: {
 	x: number;
 	y: number;
@@ -21,3 +31,37 @@ const doublePointv2 = (point: Point): Point => {
 	//this now accepts a object called point that is of type Point and returns an object that is of type Point
 	return { x: point.x * 2, y: point.y * 2 };
 };
+
+//nested object
+
+const calculateEarnings = (song: {
+	title: string;
+	artist: string;
+	numberOfStreams: number;
+	credits: { producer: string; writer: string };
+}): number => {
+	return song.numberOfStreams * 0.333;
+}; // rather than doing this we can just pass the types
+
+const calculateEarningsv2 = (song: Song): number => {
+	//refactored version of the function above using types
+	return song.numberOfStreams * 0.333;
+};
+
+const printSong = (mySong: Song): void => {
+	//mySong is a function param with the type Song
+	console.log(`${mySong.title} - ${mySong.artist}`);
+};
+
+const mySong: Song = {
+	title: 'I want to break free',
+	artist: 'Queen',
+	numberOfStreams: 123456765432,
+	credits: {
+		producer: "I don't know",
+		writer: "I don't know writer",
+	},
+};
+const earnings = calculateEarningsv2(mySong);
+console.log(earnings);
+printSong(mySong);
