ed22c4e8591323d98dc2d643a525f008bbf21159
Samip <callmesamip@gmail.com>
2022-08-15 20:37:15 +1000

    Completed the literal excercise from Colt

---
+++ array-ts/literalType.js
@@ -0,0 +1,6 @@
+var zero = 0; //zero is of type 0
+//zero = 2; //this is not accepted because it should equal to the literal value of 0
+var mood = 'Happy';
+mood = 'Sad'; //aceepts only two literal values
+//let today: DayOfWeek = 'weds'; //this complains because this is not an accepted literal
+var today = 'Wednesday';
