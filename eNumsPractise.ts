4aadda9bd5f0a360a96221ea199de7ef8b2e12b6
Samip <callmesamip@gmail.com>
2022-08-18 16:50:56 +1000

    Added the eNUM

--- eNumsPractise.ts
+++ eNumsPractise.ts
@@ -42,4 +42,13 @@ enum orderStatus {
 
 /**
  * that will compile to  the following in JS
+ * "use strict";
+var orderStatus;
+(function (orderStatus) {
+    orderStatus[orderStatus["PENDING"] = 0] = "PENDING";
+    orderStatus[orderStatus["SHIPPED"] = 1] = "SHIPPED";
+    orderStatus[orderStatus["DELIVERED"] = 2] = "DELIVERED";
+    orderStatus[orderStatus["RETURNED"] = 3] = "RETURNED";
+})(orderStatus || (orderStatus = {}));
+
  */
