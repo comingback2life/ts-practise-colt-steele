a7acfc3f38f9f9d00241c7ed24d3b063bc07a530
Samip <callmesamip@gmail.com>
2022-09-28 12:53:02 +1000

    Created two interface with readonly and optional modifiers

---
+++ interfacesPractise/interfaces.js
@@ -0,0 +1,18 @@
+//learning interfaces
+// type Point = {
+// 	x: number;
+// 	y: number;
+// };
+var anotherPoint = { x: 3, y: 3 };
+var Thomas = {
+    id: 1122,
+    firstName: 'Thomas',
+    lastName: 'Thomas',
+    nickname: 'Tom'
+};
+var Jerry = {
+    id: 1212,
+    firstName: 'Jerry',
+    lastName: 'Thomas'
+};
+console.table(Jerry);
--- interfacesPractise/interfaces.ts
+++ interfacesPractise/interfaces.ts
@@ -11,3 +11,24 @@ interface Point {
 	y: number;
 }
 const anotherPoint: Point = { x: 3, y: 3 };
+
+interface Person {
+	readonly id: number;
+	firstName: string;
+	lastName: string;
+	nickname?: string; //nickname is optional
+}
+
+const Thomas: Person = {
+	id: 1122,
+	firstName: 'Thomas',
+	lastName: 'Thomas',
+	nickname: 'Tom',
+};
+const Jerry: Person = {
+	id: 1212,
+	firstName: 'Jerry',
+	lastName: 'Thomas',
+};
+
+console.table(Jerry);
