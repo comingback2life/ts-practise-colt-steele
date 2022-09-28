5a1d67e6b60abaf100f904dc0ba4b64c70764dfb
Samip <callmesamip@gmail.com>
2022-09-28 13:11:13 +1000

    Extending Interfaces

---
+++ interfacesPractise/reOpeningInterfaces.js
@@ -0,0 +1,17 @@
+var elton = {
+    name: 'Elton',
+    age: 0.5,
+    breed: 'Chihuwawa',
+    bark: function () {
+        return 'Woof!';
+    }
+}; //dog is the combination of two interfaces
+var chewy = {
+    name: 'Chewy',
+    age: 30,
+    breed: 'Lab',
+    bark: function () {
+        return 'Barkkkksss !';
+    },
+    job: 'drug sniffer'
+};
--- interfacesPractise/reOpeningInterfaces.ts
+++ interfacesPractise/reOpeningInterfaces.ts
@@ -15,3 +15,17 @@ const elton: Dog = {
 		return 'Woof!';
 	},
 }; //dog is the combination of two interfaces
+
+interface serviceDogs extends Dog {
+	job: 'drug sniffer' | 'bomb squad' | 'guide dog';
+}
+
+const chewy: serviceDogs = {
+	name: 'Chewy',
+	age: 30,
+	breed: 'Lab',
+	bark: () => {
+		return 'Barkkkksss !';
+	},
+	job: 'drug sniffer',
+};
