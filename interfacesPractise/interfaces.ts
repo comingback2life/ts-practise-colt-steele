88800e0cf9015fe013fb738efd4e33c68654f469
Samip <callmesamip@gmail.com>
2022-09-28 12:49:15 +1000

    Learning Interfaces

--- interfacesPractise/interfaces.ts
+++ interfacesPractise/interfaces.ts
@@ -1,1 +1,13 @@
 //learning interfaces
+// type Point = {
+// 	x: number;
+// 	y: number;
+// };
+
+// const anotherPoint: Point = { x: 3, y: 2 };
+
+interface Point {
+	x: number;
+	y: number;
+}
+const anotherPoint: Point = { x: 3, y: 3 };
