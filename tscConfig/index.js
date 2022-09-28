4855261922f9e35ba87d58768a60082b1856cda7
Samip <callmesamip@gmail.com>
2022-09-28 13:41:37 +1000

    Learning watch mode

---
+++ tscConfig/index.js
@@ -0,0 +1,6 @@
+var newChicken = {
+    breed: 'Silkie',
+    eggsPerWeek: 10,
+    name: 'Elton',
+    age: 2
+};
---
+++ tscConfig/index.ts
@@ -0,0 +1,13 @@
+interface Chicken {
+	breed: string;
+	eggsPerWeek: number;
+	name: string;
+	age: number;
+}
+
+const newChicken: Chicken = {
+	breed: 'Silkie',
+	eggsPerWeek: 10,
+	name: 'Elton',
+	age: 2,
+};
